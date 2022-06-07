a98290b0edeb8bec95c37b62a1e6cb69f2f6a8f6
EugeneStone <89702595+EugeneStone@users.noreply.github.com>
2022-06-07 18:11:58 +0300

    2

--- script.js
+++ script.js
@@ -1,70 +1,32 @@
-let button = document.querySelector('.button');
-let buttonDelete = document.querySelector('.buttonDelete');
+let button = document.getElementById('button');
+let buttonDelete = document.getElementById('buttonDelete');
 
 
     
 function drawHystogram(){
-    let numberArray;
-    let labelArray;
-    let backColorArray;
-    let bordColorArray;
-    let counter;
-//Разделем строку чисел из input на массив чисел
-    let numbers = document.querySelector('.numbers');
-    counter = numbers.value.length;
-    numberArray = numbers.value.split('');
-    numberArray.forEach(element => {
-        Number(element);
-    });
-//создаем массив надписей
-    labelArray = new Array();
-    for(let i = 1; i <= counter; i++){
-        
-        labelArray.push(String(i));
-    }
- // массив цвета столбца
-    backColorArray = new Array();
-    for(let i = 0; i < counter; i++){
-        
-        backColorArray.push('rgba(255, 99, 132, 0.2)');
-    }
+      
+    let numbers = document.getElementById('numbers');
+    let myTable = document.getElementById("table");
+    let numberArray = numbers.value.split('');
+       
+    for (let i = 0; i < numberArray.length; i++){
+      let newTr = document.createElement('tr');
+      myTable.append(newTr);
 
-     // массив границы столбца
-    bordColorArray = new Array();
-    for(let i = 0; i < counter; i++){
-        
-        bordColorArray.push('rgba(255, 99, 132, 1)');
-    }
-  
-    //взято из документации chart.js
-const ctx = document.getElementById('myChart').getContext('2d');
-    const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: labelArray,
-        datasets: [{
-            label: 'Числа',
-            data: numberArray,
-            backgroundColor: backColorArray,
-            borderColor: bordColorArray,
-            borderWidth: 1
-        }]
-    },
-    options: {
-        
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+    for (let j = 0; j < numberArray[i]; j++){
+      let newTd = document.createElement('td');
+        newTd.className = 'td';
+        newTr.append(newTd);
+    
+    }}     
+ 
 }
 
+button.addEventListener('click',  drawHystogram);
 
-
-
-button.addEventListener('click', ()=> {drawHystogram()});
-buttonDelete.addEventListener('click', ()=> document.location.reload())
-
-
+buttonDelete.addEventListener('click', ()=>{ 
+let myTable = document.getElementById("table");
+    while (myTable.firstChild) {
+        myTable.removeChild(myTable.firstChild);
+}}
+);
