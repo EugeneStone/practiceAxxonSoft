656e50f09cef53c52140b864d728695538ec4888
EugeneStone <89702595+EugeneStone@users.noreply.github.com>
2022-06-13 09:11:18 +0300

    Поправлены классы. Добавлен innerText. Форматирование.

--- script.js
+++ script.js
@@ -3,57 +3,65 @@ const buttonDraw = document.getElementById('button');
 const buttonDelete = document.getElementById('buttonDelete');
 
 
-function validator(){
-const inputNumbers = document.getElementById('numbers');
+function validator() {
+  const inputNumbers = document.getElementById('numbers');
 
-  if (!inputNumbers.value){
-        alert('Поле ввода не должно быть пустым');
+  if (!inputNumbers.value) {
+    alert('Поле ввода не должно быть пустым');
   }
-  else{ 
-    const numbersArray = inputNumbers.value.replace(/\s+/g, '').trim().split('');
-    
-      for (let i = 0; i < numbersArray.length; i++){
-          if( !Number.isInteger(Number(numbersArray[i]))){
-           alert('Ввод должен содержать только числа');
-             return;
-           }
-         }
-      
-        drawHistogram();
-      
-  }
-}   
+  else {
+    const numbersArray = inputNumbers.value
+      .replace(/\s+/g, '')
+      .trim()
+      .split('');
 
-
-function drawHistogram(){
-      
-    const inputNumbers = document.getElementById('numbers');
-     const divContainer = document.getElementById('ContainerHistogram');
-     const numbersArray = inputNumbers.value.replace(/\s+/g, ' ').trim().split(' ');
-      
-       
-    for (let i = 0; i < numbersArray.length; i++){
-      const newColumn = document.createElement('div');
-       newColumn.innerHTML = numbersArray[i];
-        newColumn.className ='Column';
-         newColumn.style.height = `${numbersArray[i]}px`;
-          divContainer.append(newColumn);
-
+    for (let i = 0; i < numbersArray.length; i++) {
+      if (!Number.isInteger(Number(numbersArray[i]))) {
+        alert('Ввод должен содержать только числа');
+        return;
+      }
     }
-    
+
+    drawHistogram();
+
+  }
 }
-function clearHistogram(){
-  const columnsForDelete = document.querySelectorAll('.Column');
-   columnsForDelete.forEach(elem => elem.remove());
-   
+
+
+function drawHistogram() {
+
+  const inputNumbers = document.getElementById('numbers');
+  const divContainer = document.getElementById('сontainerHistogram');
+  const numbersArray = inputNumbers.value
+    .replace(/\s+/g, ' ')
+    .trim()
+    .split(' ');
+
+
+  for (let i = 0; i < numbersArray.length; i++) {
+    const newColumn = document.createElement('div');
+    newColumn.innerText = numbersArray[i];
+    newColumn.className = 'сolumn';
+    newColumn.style.height = `${numbersArray[i]}px`;
+    divContainer.append(newColumn);
+
+  }
+
+}
+function clearHistogram() {
+  const columnsForDelete = document.querySelectorAll('.сolumn');
+  columnsForDelete.forEach(elem => elem.remove());
+
 
 }
 
-buttonDraw.addEventListener('click', ()=>{
-  const columnsForDelete = document.querySelectorAll('.Column');
-  if (columnsForDelete.value != ''){
-    clearHistogram();}
-    validator()});
+buttonDraw.addEventListener('click', () => {
+  const columnsForDelete = document.querySelectorAll('.сolumn');
+  if (columnsForDelete.value != '') {
+    clearHistogram();
+  }
+  validator()
+});
 
 buttonDelete.addEventListener('click', clearHistogram);
 
